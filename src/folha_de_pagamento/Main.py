6bb979de2e3c374bd5e480139c3d21d3a1668100
Pedro Henrique Olino de Borba <12925114999@ulife.com.br>
2026-09-25 20:54:01 -0300

    CRUD Feito

--- src/folha_de_pagamento/Main.py
+++ src/folha_de_pagamento/Main.py
@@ -1,4 +1,5 @@
 import Colaboradores
+import array
 import pandas as pd
 
 def CadastrarColaborador(nome_col:str, salario_col:float, tipo_col:str = "padrao", adicional:float = 0.0) -> bool:
@@ -35,14 +36,73 @@ def CadastrarColaborador(nome_col:str, salario_col:float, tipo_col:str = "padrao
                 novo_colaborador:Colaboradores.Colaborador = Colaboradores.Colaborador(0, nome_col, salario_col)
             else: novo_colaborador:Colaboradores.Colaborador = Colaboradores.Colaborador(Colaboradores.colaboradores[-1].matricula + 1, nome_col, salario_col)
 
+def MostrarColaborador(col_matricula:int):
+    
+    col:Colaboradores.Colaborador = EncontrarColaboradores(col_matricula, "")[0]
+
+    match type(col):
+        case Colaboradores.ColaboradorComissionado:
+            print(f"Nome: {col.nome}\nMatrícula: {col.matricula}\nTipo: Comissionado\nSalário Base: {col.salario_base}\nPercentual de Comissão: {col.percentual_comissao}\nValor de Vendas: {col.valor_de_vendas}\nSalário Total: {col.salario_base + (col.valor_de_vendas * (col.percentual_comissao / 100))}")
+        case Colaboradores.ColaboradorPorProducao:
+            print(f"Nome: {col.nome}\nMatrícula: {col.matricula}\nTipo: Produtividade\nSalário Base: {col.salario_base}\nValor Por Unidade Produzida: {col.valor_por_unidade_produzida}\nQuantidade Produzia: {col.quantidade_produzida}\nSalário Total: {col.salario_base + (col.quantidade_produzida * col.valor_por_unidade_produzida)}")
+        case _:
+            print(f"Nome: {col.nome}\nMatrícula: {col.matricula}\nTipo: Produtividade\nSalário Base: {col.salario_base}\nSalário Total: {col.salario_base}")
+
+
 def AlterarColaborador(matricula:int, novo_nome:str = "", novo_salario:float = 0.00, novo_tipo:str = "padrao",
                        novo_percentual_comissao:float = 0.00, novo_valor_vendas:float = 0.00,
                        novo_valor_por_producao:float = 0.00, nova_quantidade_produzida:int = 0):
     
-    pass
+    match novo_tipo.strip().lower():
+        case "padrao":
+            colaborador_modificado:Colaboradores.Colaborador = Colaboradores.Colaborador(matricula, novo_nome, novo_salario)
+            for col in Colaboradores.colaboradores:
+                if col.matricula == matricula:
+                    col = colaborador_modificado
+        case "comissionado":
+            colaborador_modificado:Colaboradores.Colaborador = Colaboradores.ColaboradorComissionado(matricula, novo_nome, novo_salario, novo_percentual_comissao)
+            colaborador_modificado.valor_de_vendas = novo_valor_vendas
+            for col in Colaboradores.colaboradores:
+                if col.matricula == matricula:
+                    col = colaborador_modificado
+        case "produtividade":
+            colaborador_modificado:Colaboradores.Colaborador = Colaboradores.ColaboradorPorProducao(matricula, novo_nome, novo_salario, novo_valor_por_producao)
+            colaborador_modificado.quantidade_produzida = nova_quantidade_produzida
+            for col in Colaboradores.colaboradores:
+                if col.matricula == matricula:
+                    col = colaborador_modificado
 
+def ExcluirColaborador(col_matricula:int) -> bool:
 
-def MostrarColaboradores(nome_arquivo:str, inicio:int = 0, qntd:int = 500) -> None:
+    for col in Colaboradores.colaboradores:
+        if col.matricula == col_matricula:
+            Colaboradores.colaboradores.remove(col)
+            return True
+    
+    return False
+
+def EncontrarColaboradores(buscar_matricula:int, buscar_nome:str) -> array:
+    
+    colaboradores_achados:array = []
+
+    #busca colaborador pela matricula
+    for col in Colaboradores.colaboradores:
+        if col.matricula == buscar_matricula:
+            colaboradores_achados.append(col)
+            break
+    
+    #busca colaborador pelo nome
+    for col in Colaboradores.colaboradores:
+        if col.nome.find(buscar_nome):
+            if col.nome == buscar_nome:
+                colaboradores_achados.clear()
+                colaboradores_achados.append(col)
+                break
+            colaboradores_achados.append(col)
+
+    return colaboradores_achados
+
+def GerarTabelaDeColaboradores(nome_arquivo:str, inicio:int = 0, qntd:int = 500) -> None:
 
     lista_de_colaboradores:dict = {
         "Matrícula": [],
@@ -98,5 +158,3 @@ def MostrarColaboradores(nome_arquivo:str, inicio:int = 0, qntd:int = 500) -> No
     df = pd.DataFrame(lista_de_colaboradores)
 
     df.to_excel(f"Sistema-Simples-de-Folha-de-Pagamento/src/output/{nome_arquivo}.xlsx")
-
-
